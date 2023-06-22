d31c8f038dde094cbacb0a5fd638d8196d0a84c1
Teal Dulcet <tdulcet@pdx.edu>
2023-06-22 05:18:00 -0700

    Added support for sorting the words.

--- script.js
+++ script.js
@@ -3,55 +3,107 @@
 const WIKTIONARY = "https://en.wiktionary.org/wiki/";
 const WIKIPEDIA = "https://en.wikipedia.org/wiki/";
 
-const words1 = new Set();
-const words2 = new Set();
-const words3 = new Set();
-const words4 = new Set();
-const wikis = new Set();
+const wiktionary = [];
 
 const numberFormat = new Intl.NumberFormat();
+
+const numberFormat1 = new Intl.NumberFormat([], { style: "unit", unit: "second", unitDisplay: "long" });
 
 const formatter1 = new Intl.ListFormat([], { style: "short" });
 const formatter2 = new Intl.ListFormat([], { style: "narrow" });
 
 const awords = Array.from(document.getElementsByName("words"));
 const awiki = document.getElementById("wiki");
+const asort = document.getElementById("sort");
+const adirection = document.getElementById("direction");
 const amax = document.getElementById("max");
 
+const re1 = /^[\p{Ll}'-]+$/u;
+const re2 = /^[\p{Alpha}'-]+$/u;
+const re3 = /\p{Ll}/u;
+const re4 = /\p{Alpha}/u;
+
+
+function outputtime(time) {
+	return numberFormat1.format(time / 1000);
+}
 
 function settings() {
+	const label = "update";
+	console.time(label);
+
+	const start = performance.now();
+	const table = document.getElementById("table");
+
 	const words = parseInt(awords.find((r) => r.checked).value, 10);
 	const wiki = awiki.checked;
+	const sort = parseInt(asort.value, 10);
+	const direction = adirection.checked;
 	const max = amax.valueAsNumber;
 
-	let word = null;
+	const rows = Array.from(wiktionary);
+
+	if (sort >= 0) {
+		let compare = null;
+
+		switch (sort) {
+			case 0:
+			case 2:
+			case 3:
+			case 4:
+				compare = direction ? (a, b) => a[sort][0]?.localeCompare(b[sort][0]) : (a, b) => b[sort][0]?.localeCompare(a[sort][0]);
+				break;
+			case 1:
+				compare = direction ? (a, b) => a[sort] - b[sort] : (a, b) => b[sort] - a[sort];
+				break;
+		}
+
+		rows.sort(compare);
+	}
+
+	console.timeLog(label);
+
+	let test = null;
 
 	switch (words) {
 		case 1:
-			word = words1;
+			test = (awords) => awords.some((word) => re1.test(word)) && awords.some((word) => re3.test(word));
 			break;
 		case 2:
-			word = words2;
+			test = (awords) => awords.some((word) => re2.test(word)) && awords.some((word) => re3.test(word));
 			break;
 		case 3:
-			word = words3;
+			test = (awords) => awords.some((word) => re2.test(word)) && awords.some((word) => re4.test(word));
 			break;
 		case 4:
-			word = words4;
+			test = (awords) => true;
 			break;
 	}
 
 	let count = 0;
 
-	for (const element of words4) {
-		const show = word.has(element) && (!wiki || wikis.has(element));
-		element.style.display = show && count < max ? "" : "none";
+	for (const [awords, anum, aforms, apos, awiki, row] of rows) {
+		const show = test(awords) && (!wiki || awiki.length);
+		row.style.display = show && count < max ? "" : "none";
+		if (sort >= 0) {
+			table.append(row);
+		}
 		if (show) {
 			++count;
 		}
 	}
 
-	document.getElementById("total").textContent = `${numberFormat.format(count)} / ${numberFormat.format(words4.size)}${count > max ? ` (Only the first ${numberFormat.format(max)} shown)` : ""}`;
+
+	console.timeLog(label);
+
+	document.getElementById("total").textContent = `${numberFormat.format(count)} / ${numberFormat.format(rows.length)}${count > max ? ` (Only the first ${numberFormat.format(max)} shown)` : ""}`;
+
+	const end = performance.now();
+	const time = outputtime(end - start);
+	document.getElementById("update").textContent = time;
+	console.log(`The Wiktionary table was updated in ${time}.`);
+
+	console.timeEnd(label);
 }
 
 function createlink(link) {
@@ -61,7 +113,7 @@ function createlink(link) {
 	return a;
 }
 
-addEventListener("load", (event) => {
+addEventListener("load", async (event) => {
 	const wikt = document.getElementById("wiktionary");
 
 	wikt.textContent = "Loading…";
@@ -71,13 +123,22 @@ addEventListener("load", (event) => {
 
 	const table = clone.getElementById("table");
 
-	fetch("wiktionary/Wiktionary words.tsv").then(async (response) => {
+	const label = "load";
+	console.time(label);
+
+	const start = performance.now();
+
+	await fetch("wiktionary/Wiktionary words.tsv").then(async (response) => {
 		if (response.ok) {
 			const text = await response.text();
 			// console.log(text);
 
+			console.timeLog(label);
+
 			const data = text.split("\n").filter((r) => r.length).map((r) => r.split("\t"));
 			// console.log(data);
+
+			console.timeLog(label);
 
 			for (const [key, words, num, forms, pos, wiki] of data) {
 				const row = table.insertRow();
@@ -91,9 +152,11 @@ addEventListener("load", (event) => {
 				}));
 
 				cell = row.insertCell();
-				cell.textContent = numberFormat.format(parseInt(num, 10));
+				const anum = parseInt(num, 10);
+				cell.textContent = numberFormat.format(anum);
 
 				cell = row.insertCell();
+				const aforms = forms.split(",");
 				/* cell.innerHTML = formatter1.format(forms.split(",").map((x) => {
 					if (words.has(x)) {
 						const link = createlink(`${WIKTIONARY}${x}`);
@@ -102,10 +165,11 @@ addEventListener("load", (event) => {
 					}
 					return x;
 				})); */
-				cell.textContent = formatter1.format(forms.split(","));
+				cell.textContent = formatter1.format(aforms);
 
 				cell = row.insertCell();
-				cell.textContent = formatter2.format(pos.split(","));
+				const apos = pos.split(",");
+				cell.textContent = formatter2.format(apos);
 
 				cell = row.insertCell();
 				const awiki = wiki.split(/("[^"]+"|[^",]*)(?:,|$)/u).filter((x, i) => i % 2 !== 0).map((x) => x.startsWith('"') && x.endsWith('"') ? x.slice(1, -1) : x);
@@ -115,29 +179,20 @@ addEventListener("load", (event) => {
 					return link.outerHTML;
 				}));
 
-				const test1 = awords.some((word) => /^[\p{Ll}'-]+$/u.test(word));
-				const test2 = awords.some((word) => /^[\p{Alpha}'-]+$/u.test(word));
-				const test3 = awords.some((word) => /\p{Ll}/u.test(word));
-				const test4 = awords.some((word) => /\p{Alpha}/u.test(word));
-
-				if (test1 && test3) {
-					words1.add(row);
-				}
-				if (test2 && test3) {
-					words2.add(row);
-				}
-				if (test2 && test4) {
-					words3.add(row);
-				}
-				words4.add(row);
-
-				if (awiki.length) {
-					wikis.add(row);
-				}
+				wiktionary.push([awords, anum, aforms, apos, awiki, row]);
 			}
 		}
 
+		console.timeLog(label);
+
 		wikt.replaceChildren(clone);
+
+		const end = performance.now();
+		const time = outputtime(end - start);
+		document.getElementById("load").textContent = time;
+		console.log(`The Wiktionary dictionary data was loaded in ${time}.`);
+
+		console.timeLog(label);
 
 		settings();
 	}).catch((error) => {
@@ -145,6 +200,8 @@ addEventListener("load", (event) => {
 
 		wikt.textContent = `Error: ${error}`;
 	});
+
+	console.timeEnd(label);
 });
 
 for (const radio of awords) {
@@ -152,4 +209,6 @@ for (const radio of awords) {
 }
 
 awiki.addEventListener("change", settings);
+asort.addEventListener("change", settings);
+adirection.addEventListener("change", settings);
 amax.addEventListener("change", settings);
